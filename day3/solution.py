4e435a753497576b7459878aa44f1efda11d3c29
Jen Gori <jgori@hotmail.co.uk>
2025-12-03 12:07:38 +0000

    Add day 3 solution

---
+++ day3/solution.py
@@ -0,0 +1,39 @@
+def get_input(filename):
+    with open(filename) as f:
+        banks = [[int(n) for n in line] for line in f.read().splitlines()]
+        return banks
+
+def solve_part_1(banks):
+
+    solution = 0
+
+    for bank in banks:
+        tens = max(bank[:-1])
+        units = max(bank[bank.index(tens)+1:])
+        n = 10 * tens + units
+        solution += n
+
+    return solution
+
+def solve_part_2(banks):
+
+    total_joltage = 0
+
+    for bank in banks:
+
+        joltage = 0
+        position = 0
+
+        for i in range(12):
+            n = max(bank[position:i-11]) if i < 11 else max(bank[position:])
+            position += bank[position:].index(n) + 1
+            joltage += n * 10**(11-i)
+
+        total_joltage += joltage
+
+    return total_joltage
+
+banks_ = get_input("input.txt")
+
+print(f"Solution for part 1: {solve_part_1(banks_)}")
+print(f"Solution for part 2: {solve_part_2(banks_)}")
